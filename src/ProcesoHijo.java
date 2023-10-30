68cbc6af20b9e8bba6f7af9f37109dc187c42e97
ABlazquezM <andrea.blazquez.martin@gmail.com>
2023-10-30 20:20:27 +0100

    Tarea clase Ejecuta

--- src/ProcesoHijo.java
+++ src/ProcesoHijo.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
 public class ProcesoHijo {
-
 	public static void main(String[] args) {
-
-		Scanner sc  = new Scanner(System.in);
-		String cadena = sc.nextLine();
+		Scanner lector = new Scanner(System.in);
+		String cadena= lector.nextLine();
 		
-		while(cadena != null) {
-			System.out.println("Proceso Hijo: "+cadena.toUpperCase());
-			cadena = sc .nextLine();
+		while(cadena!= null) {
+			System.out.println("Proceso hijo: "+ cadena.toUpperCase());
+			cadena=lector.nextLine();
 		}
 	}
-
 }
--- src/ProcesoPadre.java
+++ src/ProcesoPadre.java
@@ -1,58 +1,41 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class ProcesoPadre {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		// Creamos el proceso hijo
+		// crear proceso hijo
 		try {
-			Process hijo = new ProcessBuilder("java", "-jar", "C:\\Prueba\\miProcesoHijo.jar").start();
-
-			// Lectura por teclado de la info a envia al proceso hijo
-			Scanner sc = new Scanner(System.in);
-
-			// Obtenemos la entreada standard del proceso del hijo (stdin)
-			InputStream is = hijo.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-			// Salida standrad del proceso (stdout)
-			// PrintStream ps = new PrintStream(hijo.getOutputStream());
-			OutputStream os = hijo.getOutputStream();
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
-
-			String cadena = "";
-
-			while (!cadena.equalsIgnoreCase("fin")){
-				// Lectura de la cadena de caracteres por teclado
-				System.out.println("Introduce una cadea de caracteres: ");
-				cadena = sc.nextLine();
-
-				// Se envía la cadena de caracteres proceso hijo
-				bw.write(cadena); 
-				bw.newLine();
-				// ps.println(cadena);
-
-				// Volcado de toda la info
-				bw.flush();
-				// ps.flush();
-
-				// Lectura de salida del proceso hijo
-				System.out.println(br.readLine());
-
-			}
-			System.out.println("Proceso finalizado");
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		Process hijo = new ProcessBuilder("java","-jar","C:\\Prueba\\nuevo.jar").start();
+		//lectura por teclado de la infroamcion a enviar a proceso hijo
+		Scanner in = new Scanner(System.in);
+		
+		//Obtenemos la entrada estandard del proceso hijo(stdjn)
+		BufferedReader br= new BufferedReader(new InputStreamReader(hijo.getInputStream()));
+		
+		//obtener la salida estandard del proceso hijo (stdout)
+		PrintStream ps= new PrintStream(hijo.getOutputStream());
+		
+		String cadena="";
+		
+		while(!cadena.equalsIgnoreCase("fin")) {
+			//lectura de la cadena de caracteres
+			System.out.println("Introduzca una cadena de caracteres: ");
+			cadena=in.nextLine();
+			//Se envia la cadena de caracteres proceso hijo
+			ps.println(cadena);
+			//volcamos toda la informacion
+			ps.flush();
+			//lectura de la salida del proceso hijo
+			System.out.println(br.readLine());
+		}
+		System.out.println("Proceso finalizado");
+		
+		
+		}catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
